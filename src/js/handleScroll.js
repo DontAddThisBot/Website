a2c8dd86597098e7c841632e16468c90cdcce563
Kattah <kattahpro@gmail.com>
2022-11-23 05:34:45 +0900

    lowered timer

--- src/js/handleScroll.js
+++ src/js/handleScroll.js
@@ -2,32 +2,32 @@ export const handleScroll = () => {
   const topWrapper = document.getElementById("top-wrapper");
   const bottomWrapper = document.getElementById("bottom-wrapper");
   for (const wrappers of [topWrapper, bottomWrapper]) {
-    if (window.scrollY > 200) {
+    if (window.scrollY) {
       window.removeEventListener("scroll", handleScroll);
-      topWrapper.style.transform = `translateX(-20%)`;
-      bottomWrapper.style.transform = `translateX(20%)`;
-      wrappers.style.transition = "transform 0.8s ease-in-out";
+      topWrapper.style.transform = `translateX(-2%)`;
+      bottomWrapper.style.transform = `translateX(2%)`;
+      wrappers.style.transition = "transform 0.2s ease-in-out";
 
       setTimeout(() => {
         wrappers.style.transform = "translate3d(0, 0, 0)";
         wrappers.style.opacity = "0.2";
-      }, 800);
+      }, 200);
 
       setTimeout(() => {
         wrappers.style.opacity = "0.4";
-      }, 850);
+      }, 250);
 
       setTimeout(() => {
         wrappers.style.opacity = "0.6";
-      }, 900);
+      }, 260);
 
       setTimeout(() => {
         wrappers.style.opacity = "0.8";
-      }, 950);
+      }, 270);
 
       setTimeout(() => {
         wrappers.style.opacity = "1";
-      }, 1000);
+      }, 280);
     }
   }
 };
