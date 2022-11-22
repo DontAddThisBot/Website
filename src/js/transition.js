6bc382e78c1f3c982dd7c6996e3b2a7f42457f44
Kattah <kattahpro@gmail.com>
2022-11-23 05:33:25 +0900

    removed 2 timeouts

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,20 +1,14 @@
 export function transition(isNegative, ...streamerInfo) {
   for (const classes of [...streamerInfo]) {
-    classes[0].style.transform = `translate3d(${
-      isNegative ? "-" : ""
-    }25%, 0, 0)`;
-    classes[0].style.transition = "transform 0.5s ease-in-out";
-
-    setTimeout(() => {
-      classes[0].style.transform = "translate3d(0, 0, 0)";
-    }, 500);
-
-    setTimeout(() => {
-      classes[0].style.transition = "none";
-    }, 1000);
-
-    setTimeout(() => {
+    if (classes) {
+      classes[0].style.transform = `translate3d(${
+        isNegative ? "-" : ""
+      }10%, 0, 0)`;
       classes[0].style.transition = "transform 0.5s ease-in-out";
-    }, 1001);
+
+      setTimeout(() => {
+        classes[0].style.transform = "translate3d(0, 0, 0)";
+      }, 500);
+    }
   }
 }
