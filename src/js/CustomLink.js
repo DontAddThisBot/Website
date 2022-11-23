cedda6a9d4a1ae4ef471f8a176378294ebf62c5f
Kattah <kattahpro@gmail.com>
2022-11-23 14:51:07 +0900

    updated Link path

--- src/js/CustomLink.js
+++ src/js/CustomLink.js
@@ -1,11 +1,8 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const match = useMatch({ path: resolvedPath.pathname, end: true });
-
+export function CustomLink({ className, to, children, ...props }) {
   return (
-    <li className={match ? "active" : ""}>
+    <li className={className}>
       <Link to={to} {...props}>
         {children}
       </Link>
