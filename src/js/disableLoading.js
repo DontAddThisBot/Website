55b557d21ceca9d02adc747632ff6849232594d5
Kattah <kattahpro@gmail.com>
2022-11-23 14:50:57 +0900

    removed short useless js function

--- src/js/disableLoading.js
+++
@@ -1,7 +0,0 @@
-export async function disableLoading() {
-  const loading = document.getElementsByClassName("loading");
-  loading[0].parentNode.removeChild(loading[0]);
-  console.log("button disabled");
-
-  return true;
-}
