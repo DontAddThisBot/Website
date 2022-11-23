5613344e3400c43fd8b26bf366a62f913dfb1e85
Kattah <kattahpro@gmail.com>
2022-11-24 04:28:01 +0900

    removed tolocalestring?

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -318,13 +318,11 @@ export default function Home() {
       </BottomWrapper>
       <StreamerText>
         <div className="streamer-bot-info">Who is using DontAddThisBot?</div>
-        <p>{totalMods?.total.toLocaleString()} Total Modded Channels</p>
+        <p>{totalMods?.total} Total Modded Channels</p>
         <p className="modded-follows">
-          {totalMods?.follows.toLocaleString()} Total Modded Follows
+          {totalMods?.follows} Total Modded Follows
         </p>
-        <p className="modded-views">
-          {totalMods?.views.toLocaleString()} Total Modded Views
-        </p>
+        <p className="modded-views">{totalMods?.views} Total Modded Views</p>
       </StreamerText>
       <StreamerBox>
         <button
