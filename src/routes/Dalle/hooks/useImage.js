46345be46d2855d8ad7e5352a98e3cbb55ad8eaa
Kattah <kattahpro@gmail.com>
2022-12-24 15:53:47 +0900

    API

--- src/routes/Dalle/hooks/useImage.js
+++ src/routes/Dalle/hooks/useImage.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 export default function useImage(ID) {
 	const [image, setImage] = useState([]);
 	useEffect(() => {
-		fetch(`https://poros.lol/api/bot/dalle/${ID}`)
+		fetch(`https://api.poros.lol/api/bot/dalle/${ID}`)
 			.then((res) => res.json())
 			.then((data) => {
 				setImage(data);
