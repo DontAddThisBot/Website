83052054201754b09b93ffc8f35e13992e719c04
Kattah <kattahpro@gmail.com>
2022-12-24 15:51:44 +0900

    Changed Localhost

--- src/routes/Dalle/hooks/useImage.js
+++ src/routes/Dalle/hooks/useImage.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 export default function useImage(ID) {
 	const [image, setImage] = useState([]);
 	useEffect(() => {
-		fetch(`http://localhost:3003/api/bot/dalle/${ID}`)
+		fetch(`http://poros.lol/api/bot/dalle/${ID}`)
 			.then((res) => res.json())
 			.then((data) => {
 				setImage(data);
