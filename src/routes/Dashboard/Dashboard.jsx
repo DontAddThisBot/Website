bf961b58d93033529ce4c21c29246870f2002ecf
Kattah <kattahpro@gmail.com>
2023-01-02 08:27:46 +0900

    test

--- src/routes/Dashboard/Dashboard.jsx
+++ src/routes/Dashboard/Dashboard.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Dashboard = ({ test }) => {
-  const { id } = useParams();
-  console.log(test);
-  return (
-    <div className="dashboard">
-      <h1>TODO Dashboard {id}</h1>
-    </div>
-  );
+	const { id } = useParams();
+	console.log(test);
+	return (
+		<div className="dashboard">
+			<h1>TODO Dashboard {id} test</h1>
+		</div>
+	);
 };
 
 export default Dashboard;
